f41762940da2e7c837e31e0bd43f68a8a08af3aa
Abhay Kumar <abhaylagah01@gmail.com>
2023-08-07 20:27:14 +0100

    NO errors and some warnings.The application is not running.
    Whenever an operation is selected, the app is closed.

--- app/src/main/java/com/example/mycalendra/MainActivity.java
+++ app/src/main/java/com/example/mycalendra/MainActivity.java
@@ -9,11 +9,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonadd, buttonsub, buttonmult, buttondiv;
     EditText editTextN1, editTextN2;
-    TextView textview;
+    TextView textView;
     int num1, num2;
 
 
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttondiv = findViewById(R.id.btn_div);
         editTextN1 = findViewById(R.id.number1);
         editTextN2 = findViewById(R.id.number2);
-        textview = findViewById(R.id.answer);
+        textView = findViewById(R.id.answer);
 
         buttonadd.setOnClickListener(this);
         buttonsub.setOnClickListener(this);
@@ -55,33 +57,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (operation) {
             case "add":
-                textview.setText("Answer = " + (num1 + num2));
+                textView.setText("Answer = " + (num1 + num2));
                 break;
 
             case "sub":
-                textview.setText("Answer = " + (num1 - num2));
+                textView.setText("Answer = " + (num1 - num2));
                 break;
 
             case "mult":
-                textview.setText("Answer = " + (num1 * num2));
+                textView.setText("Answer = " + (num1 * num2));
                 break;
 
             case "div":
                 if (num2 != 0) {
-                    textview.setText("Answer = " + ((float) num1 / num2));
+                    textView.setText("Answer = " + ((float) num1 / num2));
                 } else {
-                    textview.setText("Answer = Infinity");
+                    textView.setText("Answer = Infinity");
                 }
                 break;
         }
     }
 }
     // This is the orignal code from the video but it had some errors.
-   /* @Override
+/*   @Override
     public void onClick(View view) {
         num1 = getInFromEditText(editTextN1);
         num2 = getInFromEditText(editTextN2);
-
         switch (view.getId()){
             case R.id.btn_add:
                 textview.setText("Answer = " + (num1 + num2));
@@ -107,4 +108,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // if (editTextN1.getText().toString().equals("")){
         //    Toast.makeText(context this, text "Enter Number", Toast.LENGTH_SHORT.show());
     }
-}*/
+}
+*/
